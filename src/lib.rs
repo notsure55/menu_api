63261d2f941cd3734e11683fdc315037da24f443
notsure55 <blackender5123@mailfence.com>
2025-05-20 17:56:48 -0700

    b

--- src/lib.rs
+++ src/lib.rs
@@ -187,7 +187,7 @@ impl Options for MenuObject {
             MenuObject::OutlineBox(b) => b.get_options(),
             MenuObject::FilledBox(b) => b.get_options(),
             MenuObject::FloatSlider(b) => b.get_options(),
-            _ => MenuOptions::new(false, false, false, false),
+            _ => MenuOptions::new(false, false, true, false),
         }
     }
 }
